0cbc5b18bda34c10c5421250601c1c3caf96ccc4
Kappaskowi <lawrence_10@hotmail.de>
2017-07-08 23:26:02 +0200

    Update money.js

--- commands/Random/money.js
+++ commands/Random/money.js
@@ -17,15 +17,34 @@ class MoneyCommand extends commando.Command {
         };
         let userDataMoney = money[message.author.id];
         message.channel.send({embed: {
-        color: 3447003,
-        author: {
-        name: "Test"
-        }
-        fields: [{
+    color: 3447003,
+    author: {
+      name: "Test",
+      //icon_url: client.user.avatarURL
+    },
+    title: "This is an embed",
+    url: "http://google.com",
+    description: "This is a test embed to showcase what they look like and what they can do.",
+    fields: [{
         name: "Fields",
         value: "They can have different fields with small headlines."
-        }]
-          });
+      },
+      {
+        name: "Masked links",
+        value: "You can put [masked links](http://google.com) inside of rich embeds."
+      },
+      {
+        name: "Markdown",
+        value: "You can put all the *usual* **__Markdown__** inside of them."
+      }
+    ],
+    timestamp: new Date(),
+    footer: {
+      //icon_url: client.user.avatarURL,
+      text: "© Example"
+    }
+  }
+});
            fs.writeFile("./json/money.json", JSON.stringify(money), (err) => {
             if (err) console.error(err)
         });
