4e92feb62c5192ade92e81f29dd989c7cb9e8e7c
Kappaskowi <lawrence_10@hotmail.de>
2017-07-08 23:01:39 +0200

    Update money.js

--- commands/Random/money.js
+++ commands/Random/money.js
@@ -16,28 +16,27 @@ class MoneyCommand extends commando.Command {
             money: 0
         };
         let userDataMoney = money[message.author.id];
-        //message.reply("Your balance is $" + userDataMoney.money);
-        /*
-        const embed = new Discord.RichEmbed()
-        .setTitle("This is your title, it can hold 256 characters")
-        .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
-        .setColor(0x00AE86)
-        .setDescription("This is the main body of text, it can hold 2048 characters.")
-        .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
-        .setImage("http://i.imgur.com/yVpymuV.png")
-        .setThumbnail("http://i.imgur.com/p2qNFag.png")
-        .setTimestamp()
-        .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
-        .addField("This is a field title, it can hold 256 characters",
-        "This is a field value, it can hold 2048 characters.")
-        .addField("Inline Field", "They can also be inline.", true)
-        .addBlankField(true)
-        .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
-        message.channel.send({embed});*/
         message.channel.send({embed: {
-        color: 3447003,
-        description: "A very simple Embed!"
-        }});
+            color: 3447003,
+            author: {
+                name: client.user.username,
+                icon_url: client.user.avatarURL
+            },
+       title: "This is an embed",
+       url: "http://google.com",
+       description: "Bank",
+       fields: [{
+        name: "Balance",
+        value: userDataMoney.money
+      }
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© Example"
+            }
+        }
+            });
            fs.writeFile("./json/money.json", JSON.stringify(money), (err) => {
             if (err) console.error(err)
         });
