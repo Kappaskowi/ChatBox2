839c969e19098d36ba800a01b736a09c20e21659
Kappaskowi <lawrence_10@hotmail.de>
2017-07-08 22:38:09 +0200

    Update money.js

--- commands/Random/money.js
+++ commands/Random/money.js
@@ -1,6 +1,6 @@
 const commando = require('discord.js-commando');
 const fs = require("fs");
-const embed = new Discord.RichEmbed();
+
 class MoneyCommand extends commando.Command {
     constructor(client) {
         super(client, {
@@ -17,8 +17,21 @@ class MoneyCommand extends commando.Command {
         };
         let userDataMoney = money[message.author.id];
         //message.reply("Your balance is $" + userDataMoney.money);
-        //embed.setTitle("Bank");
-        //embed.addField("Balance",userDataMoney.money, false);
+        const embed = new Discord.RichEmbed()
+        .setTitle("This is your title, it can hold 256 characters")
+        .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
+        .setColor(0x00AE86)
+        .setDescription("This is the main body of text, it can hold 2048 characters.")
+        .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
+        .setImage("http://i.imgur.com/yVpymuV.png")
+        .setThumbnail("http://i.imgur.com/p2qNFag.png")
+        .setTimestamp()
+        .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
+        .addField("This is a field title, it can hold 256 characters",
+        "This is a field value, it can hold 2048 characters.")
+        .addField("Inline Field", "They can also be inline.", true)
+        .addBlankField(true)
+        .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
         message.channel.send({embed});
            fs.writeFile("./json/money.json", JSON.stringify(money), (err) => {
             if (err) console.error(err)
