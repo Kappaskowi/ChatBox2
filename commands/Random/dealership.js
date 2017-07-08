d0fcac188a6b9713b742f8e6311164b74b2a46e1
Kappaskowi <lawrence_10@hotmail.de>
2017-07-09 00:59:21 +0200

    Update dealership.js

--- commands/Random/dealership.js
+++ commands/Random/dealership.js
@@ -25,10 +25,12 @@ class DealershipCommand extends commando.Command {
       });
     }
     else {
+                for(let i = 0; i < 3; i++) {
                 message.channel.send({embed: {
                 color: 3447003,
-                description: "A very simple Embed!"
-                }});   
+                description: DataDealership[i].model
+                }})
+                };   
     }
   }
 };
