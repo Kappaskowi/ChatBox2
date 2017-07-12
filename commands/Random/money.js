4576445935ab9c55cfc868fd9c2c9987a418d341
Kappaskowi <lawrence_10@hotmail.de>
2017-07-13 00:55:12 +0200

    Update money.js

--- commands/Random/money.js
+++ commands/Random/money.js
@@ -16,11 +16,32 @@ class MoneyCommand extends commando.Command {
             money: 0
         };
         let userDataMoney = money[message.author.id];
-            message.channel.send({embed: {
-             color: 3447003,
-            description: "A very simple Embed!"}}
-          );
-           fs.writeFile("./json/money.json", JSON.stringify(money), (err) => {
+        message.channel.send({
+            embed: {
+                color: 3447003,
+                description: "A very simple Embed!"
+            }
+        }
+        );
+        var rollback = function (client) {
+            //terminating a client connection will
+            //automatically rollback any uncommitted transactions
+            //so while it's not technically mandatory to call
+            //ROLLBACK it is cleaner and more correct
+            client.query('ROLLBACK', function () {
+                client.end();
+            });
+        };
+
+        pgClient.query('BEGIN', function (err, result) {
+            if (err) return rollback(pgClient);
+            pgClient.query('INSERT INTO Bank(userid,cash,bankamount,balance) VALUES('+ message.author.id +',100,1000,100,2000) WHERE id = $1', [1], function (err, result) {
+                    if (err) return rollback(pgClient);
+                    //disconnect after successful commit
+                    pgClient.query('COMMIT', pgClient.end.bind(pgClient));
+                });
+            });
+        fs.writeFile("./json/money.json", JSON.stringify(money), (err) => {
             if (err) console.error(err)
         });
     }
