6506b5ec164963ecca59f1689fb629e542d6297d
Kappaskowi <lawrence_10@hotmail.de>
2017-07-13 00:02:18 +0200

    Update test.js

--- test.js
+++ test.js
@@ -8,15 +8,17 @@ const fs = require("fs");
 const commando = require('discord.js-commando');
 const bot = new commando.Client();
 const fs = require("fs");
+var pg = require(‘pg’);
 bot.registry.registerGroup('random','Random');
 
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands");
 
-
+var connectionString = process.env.DATABASE_URL;
+var pgClient = new pg.Client(connectionString);
 //var http = require('http'); http.createServer(function (req, res) { res.writeHead(200, {'Content-Type': 'text/plain'}); res.send('it is running\n'); }).listen(process.env.PORT || 5000);
 bot.login(process.env.BOT_TOKEN);
-
+pgClient.connect();
 
 /*
 // The bot is ready
