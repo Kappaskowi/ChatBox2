a2cd69bc250f14068ad5178bf54dd60bd1c2d011
Kappaskowi <lawrence_10@hotmail.de>
2017-07-13 21:01:33 +0200

    Update money.js

--- commands/Random/money.js
+++ commands/Random/money.js
@@ -35,7 +35,7 @@ class MoneyCommand extends commando.Command {
                     },
                     fields: [{
                         name: "Cash",
-                        value: userDataMoney.cash
+                        value: userDataMoney["cash"]
                     }]
                 }
                 );
