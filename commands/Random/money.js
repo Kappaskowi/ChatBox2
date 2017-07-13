adc53674bfed61ee312aaf953ecb16dff209a550
Kappaskowi <lawrence_10@hotmail.de>
2017-07-13 22:52:47 +0200

    Socket

--- commands/Random/money.js
+++ commands/Random/money.js
@@ -55,7 +55,7 @@ class MoneyCommand extends commando.Command {
                         }
                     }
                     );
-                    pgClient.end();
+                    //pgClient.end();
                 }
             });
         });
