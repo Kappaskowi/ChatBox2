f20e19859637da45b510e637f98c5e9a950d6f01
Kappaskowi <lawrence_10@hotmail.de>
2017-07-14 22:59:10 +0200

    .

--- commands/Random/money.js
+++ commands/Random/money.js
@@ -14,13 +14,22 @@ class MoneyCommand extends commando.Command {
     };
 
     async run(message, args) {
-        //pool.query('SELECT $1::int AS number', ['2'], function (err, res) 
-        pool.query('SELECT * FROM bank', function (err, res) {
+        pool.connect(function (err, client, done) {
             if (err) {
-                return console.error('error running query', err);
+                return console.error('error fetching client from pool', err);
             }
-            console.log(res);
-            console.log('number:', res.rows[0].cash);
+
+            //use the client for executing the query
+            client.query('SELECT $1::int AS number', ['1'], function (err, result) {
+                //call `done(err)` to release the client back to the pool (or destroy it if there is an error)
+                done(err);
+
+                if (err) {
+                    return console.error('error running query', err);
+                }
+                console.log(result.rows[0].number);
+                //output: 1
+            });
         });
 
     };
