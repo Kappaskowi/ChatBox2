669f82b34133e8d271ec682847c3aac2300781ac
Kappaskowi <lawrence_10@hotmail.de>
2017-07-14 22:44:03 +0200

    pg-db

--- commands/Random/money.js
+++ commands/Random/money.js
@@ -1,7 +1,7 @@
 const commando = require('discord.js-commando');
 const fs = require("fs");
 const pg = require("pg");
-const pool = require('CHATBOX2/lib/db');
+const pool = require('pg-db');
 
 class MoneyCommand extends commando.Command {
     constructor(client) {
