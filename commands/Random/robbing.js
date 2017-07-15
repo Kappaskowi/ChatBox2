9ae69e132eb2d2e0dc85dc3011629da2c3d8b1c8
Kappaskowi <lawrence_10@hotmail.de>
2017-07-15 15:14:53 +0200

    Update robbing.js

--- commands/Random/robbing.js
+++ commands/Random/robbing.js
@@ -24,7 +24,7 @@ class RobbingCommand extends commando.Command {
             } 
             console.log("Test2");
             message.reply("You robbed $" + robbedAmmount);
-       };
+       });
         client.end();
     }
 }
