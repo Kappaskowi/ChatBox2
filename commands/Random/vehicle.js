36f7a5617162e04bf8e17ecd2101812d7e7dd213
Kappaskowi <lawrence_10@hotmail.de>
2017-07-16 22:19:02 +0200

    .

---
+++ commands/Random/vehicle.js
@@ -0,0 +1,56 @@
+const commando = require('discord.js-commando');
+const fs = require("fs");
+const pg = require("pg");
+const pool = require('pg-db');
+var connectionString = process.env.DATABASE_URL;
+class CreateCommand extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'vehicle',
+            group: 'random',
+            memberName: 'vehicle',
+            description: 'Create a car.',
+            examples: ['vehicle model img'],
+            guildOnly: true,
+            args: [
+                {
+                    key: 'model',
+                    prompt: 'Model?',
+                    type: 'string'
+                },
+                {
+                    key: 'url',
+                    prompt: 'Image-Url?',
+                    type: 'string'
+                }
+            ]
+        });
+    }
+
+    hasPermission(msg) {
+        return msg.member.hasPermission('BAN_MEMBERS');
+    }
+
+    run(msg, args) {
+        const url = args.url;
+        const model = args.model;
+         var client = new pg.Client(connectionString);
+        client.connect();
+        var query = client.query('SELECT createvehicle($1,$2)',[model,url]);
+        query.on("row", function (row, result) {
+            result.addRow(row);
+        });
+        query.on("end", function (result) {
+            console.log("Test2");
+            if (result.rows.length > 0) {
+                message.reply(result.row + " was added to the vehicle list");
+                client.end();
+            }
+            else {
+                message.reply("Error.");
+                client.end();
+            }
+        });
+    }
+};
+module.exports = CreateCommand;
