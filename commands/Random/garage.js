7c58148cb85abead2862d63a24e701724e5c281a
Kappaskowi <lawrence_10@hotmail.de>
2017-07-16 02:13:44 +0200

    .

--- commands/Random/garage.js
+++ commands/Random/garage.js
@@ -10,17 +10,44 @@ class GarageCommand extends commando.Command {
         });
     }
     async run(message, args) {
-        let garage = JSON.parse(fs.readFileSync("./garage.json", "utf8"));
-        if (!garage[message.author.id]) {
-            message.reply("Your garage is empty");
-        } else {
-            if (!garage[message.author.id]) garage[message.author.id] = {
-                vehicles: []
-            };
-            let userDataGarage = garage[message.author.id];
-            message.reply("Vehicles: " + userDataGarage.model +"\n",{file: userDataGarage.img});
-        }
+        function InsertEmbed(_rows) {
+            var userDataGarage = JSON.parse(JSON.stringify(_rows, null, "    "));
+            console.log(userDataGarage);
+            message.channel.send({
+                "embed": {
+                    "description": message.author + " **Garage**",
+                    "color": 12367392,
+                    "timestamp": new Date(),
+                    "footer": {
+                        "text": "Garage"
+                    },
+                    "fields": [
+                        {
+                            "name": "Model",
+                            "value": userDataGarage[0],
+                            "inline": true
+                        }]
+                }
+            });
+        };
+        var query = client.query('SELECT public.getgarage($1)'[message.author.id]);
+        query.on("row", function (row, result) {
+            result.addRow(row);
+            console.log("Test1");
+        });
+        query.on("end", function (result) {
+            console.log("Test2");
+            if (result.rows.length > 0) {
+                InsertEmbed(result.rows);
+                client.end();
+            }
+            else {
+                message.reply("There is no vehicle in your garage.");
+                client.end();
+            }
+        });
     }
+
 }
 
 module.exports = GarageCommand;
