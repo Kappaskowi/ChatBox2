335a42c0834f0fa8750b02c0e3398d262aabf7da
Kappaskowi <lawrence_10@hotmail.de>
2017-07-17 19:33:06 +0200

    Update Currency.js

--- structures/currency/Currency.js
+++ structures/currency/Currency.js
@@ -8,10 +8,8 @@ class Currency {
         var query = client.query("SELECT cash, bankamount FROM public.bank WHERE userid = " + user);
         query.on("row", function (row, result) {
             result.addRow(row);
-            console.log("Test1");
         });
         query.on("end", function (result) {
-            console.log("Test2");
             if (result.rows.length > 0) {
                  userDataMoney = JSON.parse(JSON.stringify(result.rows, null, "    "));
                 client.end();
