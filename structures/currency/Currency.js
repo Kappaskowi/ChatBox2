47264dc76139b62b3bd1492110b372c4873cd32b
Kappaskowi <lawrence_10@hotmail.de>
2017-07-17 22:58:57 +0200

    .

--- structures/currency/Currency.js
+++ structures/currency/Currency.js
@@ -1,30 +1,22 @@
 const pg = require("pg");
 var connectionString = process.env.DATABASE_URL;
-// class Currency {
-//      getBalance(user) {
-//     var userDataMoney = '';
-//     var client = new pg.Client(connectionString);
-//     client.connect();
-//     var query = client.query("SELECT cash, bankamount FROM public.bank WHERE userid = " + user);
-//     query.on("row", function (row, result) {
-//         result.addRow(row);
-//     });
-//     query.on("end", function (result) {
-//         if (result.rows.length > 0) {
-//             userDataMoney = JSON.parse(JSON.stringify(result.rows, null, "    "));
-//             client.end();
-//             return userDataMoney;
-//         }
-//     });
-// }
-//}
-module.exports = function(pName) {
-  return {
-    test1: function() {
-       console.log('In test 1 '+pName);
-    },
-    test2: function() {
-       console.log('In test 2 '+pName);
-    }
-  };
+module.exports = function (user) {
+    return {
+        getBalance: function () {
+            var userDataMoney = '';
+            var client = new pg.Client(connectionString);
+            client.connect();
+            var query = client.query("SELECT cash, bankamount FROM public.bank WHERE userid = " + user);
+            query.on("row", function (row, result) {
+                result.addRow(row);
+            });
+            query.on("end", function (result) {
+                if (result.rows.length > 0) {
+                    userDataMoney = JSON.parse(JSON.stringify(result.rows, null, "    "));
+                    client.end();
+                    return userDataMoney;
+                }
+            });
+        }
+    };
 };
