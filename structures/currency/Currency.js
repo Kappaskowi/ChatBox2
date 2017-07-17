64ebe0576b44fb681d737cee6084825e1b0df551
Kappaskowi <lawrence_10@hotmail.de>
2017-07-17 23:38:13 +0200

    .

--- structures/currency/Currency.js
+++ structures/currency/Currency.js
@@ -2,8 +2,8 @@ const pg = require("pg");
 var connectionString = process.env.DATABASE_URL;
 module.exports = function (user) {
     return {
+        var : userDataMoney = '',
         getBalance: function () {
-            var userDataMoney = '';
             var client = new pg.Client(connectionString);
             client.connect();
             var query = client.query("SELECT cash, bankamount FROM public.bank WHERE userid = " + user);
