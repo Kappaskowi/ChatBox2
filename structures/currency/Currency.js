35e7c1d352cae39ac775f818b6ec9d421765447f
Kappaskowi <lawrence_10@hotmail.de>
2017-07-17 19:44:46 +0200

    Update Currency.js

--- structures/currency/Currency.js
+++ structures/currency/Currency.js
@@ -1,7 +1,7 @@
 const pg = require("pg");
 var connectionString = process.env.DATABASE_URL;
 class Currency {
-    static async getBalance(user) {
+    function getBalance(user, callback) {
         var userDataMoney = 0;
         var client = new pg.Client(connectionString);
         client.connect();
@@ -14,7 +14,7 @@ class Currency {
                  userDataMoney = JSON.parse(JSON.stringify(result.rows, null, "    "));
                 client.end();
                 console.log(userDataMoney[0].cash);
-                return userDataMoney[0].cash;
+                callback(userDataMoney[0].cash);
             }
         });
         
