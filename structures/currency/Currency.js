17b0f39abea9a4af37e26b0a98da593428f2798a
Kappaskowi <lawrence_10@hotmail.de>
2017-07-18 16:03:23 +0200

    Update Currency.js

--- structures/currency/Currency.js
+++ structures/currency/Currency.js
@@ -7,10 +7,11 @@ var level = function (num) {
     var query = client.query('SELECT * from public.bank WHERE userid = 304369797930418181')
     query.on("row", function (row, result) {
         result.addRow(row);
+        console.log("DB return 2 : " + result);
         test = result;
     });
     client.end();
-    console.log("DB return : " + test);
+    console.log("DB return 2 : " + test);
     return test;
 };
 module.exports = level;
