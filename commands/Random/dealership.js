9de2ba3aa524f1d34330970242815f2ce39bc8de
Kappaskowi <lawrence_10@hotmail.de>
2017-07-18 17:30:38 +0200

    Update dealership.js

--- commands/Random/dealership.js
+++ commands/Random/dealership.js
@@ -55,10 +55,10 @@ class DealershipCommand extends commando.Command {
         if (result.rows.length > 0) {
           var userDataGarage = JSON.parse(JSON.stringify(result.rows, null, "    "));
           console.log(userDataGarage);
-          if(userDataGarage[0].buyvehicle != null) {
-          message.channel.send(message.author + " just bought a " + userDataGarage[0].buyvehicle);
+          if(userDataGarage[0].buyvehicle === "null") {
+          message.channel.send("Error"); 
           }
-          else message.channel.send("Error");
+          else message.channel.send(message.author + " just bought a " + userDataGarage[0].buyvehicle);
           client.end();
         }
       });
