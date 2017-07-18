1cbbe993ef792c171c112dfabfc9b05e5788934c
Kappaskowi <lawrence_10@hotmail.de>
2017-07-18 16:00:10 +0200

    Update Currency.js

--- structures/currency/Currency.js
+++ structures/currency/Currency.js
@@ -4,13 +4,13 @@ var level = function (num) {
     var test = '';
     var client = new pg.Client(connectionString);
     client.connect();
-    var query = client.query('SELECT * from public.bank WHERE userid = $1', [num])
+    var query = client.query('SELECT * from public.bank WHERE userid = 304369797930418181')
     query.on("row", function (row, result) {
         result.addRow(row);
         test = row;
     });
     client.end();
-    console.log("DB return : ",test);
+    console.log("DB return : " + test);
     return test;
 };
 module.exports = level;
